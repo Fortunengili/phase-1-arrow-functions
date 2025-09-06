71210953c7ea8a8376020f4db4c91be103553123
fortunengili <ngilifofs@gmail.com>
2025-09-07 01:02:50 +0300

    arrow functions

---
+++ index.js
@@ -0,0 +1,6 @@
+const divide = (parameter1, parameter2) => parameter1/parameter2;
+console.log(divide(2000,10));
+const square = (parameterA) => parameterA * parameterA;
+console.log(square);
+const add = (parameterX, parameterY) => parameterX + parameterY;
+console.log(add)
